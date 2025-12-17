a1ded51c789e067b196f1b53106318aeebc51ee5
Benedict Angelo <benbenprofessional@gmail.com>
2025-12-17 09:23:24 +0700

    fix: origin redirect on callback

--- src/app/auth/actions.ts
+++ src/app/auth/actions.ts
@@ -3,6 +3,7 @@
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 import { createClient } from '@/utils/supabase/server'
+import { headers } from 'next/headers' // <--- Add this
 
 export async function login(formData: FormData) {
   const supabase = await createClient()
@@ -26,12 +27,17 @@ export async function login(formData: FormData) {
 export async function signup(formData: FormData) {
   const supabase = await createClient()
 
+  const origin = (await headers()).get('origin')
+
   const email = formData.get('email') as string
   const password = formData.get('password') as string
 
   const { error } = await supabase.auth.signUp({
     email,
     password,
+    options: {
+      emailRedirectTo: `${origin}/auth/callback`,
+    },
   })
 
   if (error) {
